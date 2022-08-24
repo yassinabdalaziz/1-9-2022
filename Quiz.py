eaae1eccc59643c1702019afb8e53bb5b34f7461
yassinabdalaziz <111708827+yassinabdalaziz@users.noreply.github.com>
2022-08-24 22:05:58 +0200

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,30 @@
+print("hello world")
+
+for text in "banana":
+    print(text)
+
+
+    fruits = ["apple", "banana", "cherry"]
+    for edible in fruits:
+        if edible =="banana":
+            print(edible,"is equal banana")
+        else:
+            print(edible,"not equal banana")
+    break
+
+food=["apple","banana","cherry","peach","watermelon"]
+for edibles in food:
+    print(edible)
+    break
+for numbers in range(10):
+    if numbers %2 ==0:
+        print(numbers,"is even")
+    else:
+        print(numbers,"is odd")
+
+
+
+
+
+
+
